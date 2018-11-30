3a2249106f8b0df43c5c883c09cbcc8c3ccef317
hamzahalhariri <hamzah.alhariri@infomaker.se>
2018-11-30 11:07:57 +0100

    Updated plugin base with the latest from Dashboard-Plugin

--- s3.js
+++ s3.js
@@ -1,89 +1,291 @@
-const AWS = require('aws-sdk')
-const fs = require('fs')
-const exec = require('child_process').exec
-const manifest = require('./manifest.json')
+/*
+|--------------------------------------------------------------------------
+| S3 Bucket Deploy
+|--------------------------------------------------------------------------
+|
+| npm run upload:s3 will build your plugin and upload it to your bucket.
+| Read the plugin readme for full details.
+|
+*/
 
-const expectedArgs = ["bucket", "accessKeyId", "secretAccessKey"]
+const AWS = require("aws-sdk");
+const fs = require("fs");
+const exec = require("child_process").exec;
+const colors = require("colors/safe");
+const zlib = require("zlib");
 
-var args = {}
+let manifest = require("./manifest.json");
+
+var args = {};
 
 process.argv.forEach(arg => {
-  const parts = arg.split("=")
+    const parts = arg.split("=");
 
-  if (parts[0] && parts[1] && parts.indexOf(parts[0]) > -1) {
-    args[parts[0]] = parts[1]
-  }
-})
+    if (parts[0] && parts[1] && parts.indexOf(parts[0]) > -1) {
+        args[parts[0]] = parts[1];
+    }
+});
 
-console.log("\n ----------------------------")
-console.log(" Plugin S3 plugin uploader ")
-console.log(" ----------------------------\n")
+console.log("\n");
+console.log(
+    `\t ${colors.bgWhite(colors.black(" --------------------------- "))}`
+);
+console.log(
+    `\t ${colors.bgWhite(colors.black("  Plugin S3 plugin uploader  "))}`
+);
+console.log(
+    `\t ${colors.bgWhite(colors.black(" --------------------------- "))}`
+);
+console.log("\n");
 
 if (!args.accessKeyId) {
-  console.log("💥  Failed to upload plugin, missing arg accessKeyId")
-  return false
+    console.log("💥  Failed to upload plugin, missing arg accessKeyId");
+    return false;
 } else if (!args.secretAccessKey) {
-  console.log("💥  Failed to upload plugin, missing arg secretAccessKey")
-  return false
+    console.log("💥  Failed to upload plugin, missing arg secretAccessKey");
+    return false;
 } else if (!args.bucket) {
-  console.log("💥  Failed to upload plugin, missing arg bucket")
-  return false
+    console.log("💥  Failed to upload plugin, missing arg bucket");
+    return false;
 }
 
 const s3Bucket = new AWS.S3({
-  accessKeyId: args.accessKeyId,
-  secretAccessKey: args.secretAccessKey,
-})
+    accessKeyId: args.accessKeyId,
+    secretAccessKey: args.secretAccessKey
+});
 
-const pluginName = manifest.name.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();}).replace(/\s/g, '')
-const baseKey = manifest.bundle.replace(/\./g, '-').toLowerCase()
+const bundlePath = manifest.bundle.replace(/\./g, "-").toLowerCase();
+const versionPath = manifest.version.replace(/\./g, "-").toLowerCase();
+const baseKey = `${bundlePath}/${versionPath}`;
 
-s3Bucket.upload({
-  Bucket: args.bucket,
-  Key: baseKey + "/index.js",
-  Body: fs.readFileSync("dist/index.js"),
-  ContentType: "text/javascript",
-  ContentEncoding: "UTF-8",
-  ACL: 'public-read'
-}, (err, data) => {
-    if (err) {
-      console.log("💥  " + err)
-  } else {
-    console.log("\n> uploaded index > " + data.Location + "\n")
+fs.readFile("./icon.png", (err, pluginIconData) => {
+    const shouldUploadPluginIcon = !err && pluginIconData;
 
-    s3Bucket.upload({
-      Bucket: args.bucket,
-      Key: baseKey + "/style.css",
-      Body: fs.readFileSync("dist/style.css"),
-      ContentType: "text/css",
-      ContentEncoding: "UTF-8",
-      ACL: 'public-read'
-    }, (err, data) => {
-        if (err) {
-          console.log("💥  " + err)
-      } else {
+    uploadIndexJS()
+        .then(location => {
+            console.log("\n> uploaded index > " + location + "\n");
 
-        console.log("> uploaded style > " + data.Location + "\n")
+            uploadStyleCSS()
+                .then(location => console.log("> uploaded style > " + location + "\n"))
+                .catch(err => console.log("💥  " + err));
 
-        s3Bucket.upload({
-          Bucket: args.bucket,
-          Key: baseKey + "/manifest.json",
-          Body: fs.readFileSync("dist/manifest.json"),
-          ContentType: "text/css",
-          ContentEncoding: "UTF-8",
-          ACL: 'public-read'
-        }, (err, data) => {
-            if (err) {
-            console.log("💥  " + err)
-          } else {
-            console.log("> uploaded manifest > " + data.Location + "\n")
+            const uploadManifest = () => {
+                uploadManifestJSON()
+                    .then(location => {
+                        console.log("> uploaded manifest > " + location + "\n");
 
-            const path = data.Location.replace("/manifest.json", "")
+                        console.log(
+                            "🎉  Successfully uploaded " +
+                            manifest.name +
+                            " to " +
+                            location.replace("/manifest.json", "")
+                        );
+                    })
+                    .catch(err => console.log("💥  " + err));
+            };
 
-            console.log("🎉  Successfully uploaded " + manifest.name + " to " + path)
-          }
+            /**
+             * Check if icon.png is exist.
+             * Upload icon.png
+             */
+            uploadIcon(shouldUploadPluginIcon, pluginIconData).then(iconLocation => {
+                if (iconLocation) {
+                    console.log("> uploaded plugin icon > " + iconLocation + "\n");
+
+                    manifest.graphic_url = iconLocation;
+                }
+
+                /**
+                 * Check if thumbnail.png is exist.
+                 * Upload thumbnail.png
+                 */
+                uploadThumbnail().then(thumbnailLocation => {
+                    if (thumbnailLocation) {
+                        console.log(
+                            "> uploaded plugin thumbnail > " + thumbnailLocation + "\n"
+                        );
+
+                        manifest.thumbnail_url = thumbnailLocation;
+                    }
+
+                    /**
+                     * Check if markdown.md is exist.
+                     * Upload markdown.md
+                     */
+                    uploadMarkdown().then(markDownLocation => {
+                        if (markDownLocation) {
+                            console.log(
+                                "> uploaded plugin markdown > " + markDownLocation + "\n"
+                            );
+
+                            manifest.markdown_url = markDownLocation;
+                        }
+
+                        /**
+                         * Set timestamp as a build version to avoid browser cache.
+                         * buildVersion won't be stored with plugin data.
+                         */
+                        manifest.buildVersion = Date.now();
+
+                        /**
+                         * Update the manifest file with new urls
+                         * Upload manifest.json
+                         */
+                        updateManifestFile().then(() => uploadManifest());
+                    });
+                });
+            });
         })
-      }
-    })
-  }
-})
+        .catch(err => console.log("💥  " + err));
+});
+
+function updateManifestFile() {
+    return new Promise(resolve => {
+        fs.writeFile(
+            "./manifest.json",
+            JSON.stringify(manifest, null, 4),
+            "utf8",
+            err => {
+                if (!err) {
+                    resolve();
+                } else {
+                    console.log(
+                        "> failed to set manifest properties (will continue to upload manifest) \n"
+                    );
+
+                    resolve();
+                }
+            }
+        );
+    });
+}
+
+function uploadIcon(shouldUploadPluginIcon, pluginIconData) {
+    return new Promise(resolve => {
+        if (shouldUploadPluginIcon) {
+            uploadPluginIcon(pluginIconData)
+                .then(iconLocation => resolve(iconLocation))
+                .catch(err => {
+                    console.log("💥  " + err);
+
+                    resolve(null);
+                });
+        } else {
+            resolve(null);
+        }
+    });
+}
+
+function uploadThumbnail() {
+    return new Promise(resolve => {
+        fs.readFile("./thumbnail.png", (err, pluginThumbnail) => {
+            if (!err && pluginThumbnail) {
+                upload({
+                    resolve: resolve,
+                    reject: err => {
+                        console.log("💥  " + err);
+                        resolve(null);
+                    },
+                    key: "thumbnail.png",
+                    data: pluginThumbnail,
+                    contentType: "image/png"
+                });
+            } else {
+                resolve(null);
+            }
+        });
+    });
+}
+
+function uploadIndexJS() {
+    return new Promise((resolve, reject) => {
+        zlib.gzip(fs.readFileSync("dist/index.js"), (error, result) => {
+            if (error) throw error;
+
+            upload({
+                resolve: resolve,
+                reject: reject,
+                key: "index.js",
+                data: result,
+                contentType: "text/javascript",
+                contentEncoding: "gzip"
+            });
+        });
+    });
+}
+
+function uploadStyleCSS() {
+    return new Promise((resolve, reject) => {
+        zlib.gzip(fs.readFileSync("dist/style.css"), (error, result) => {
+            if (error) throw error;
+
+            upload({
+                resolve: resolve,
+                reject: reject,
+                key: "style.css",
+                data: result,
+                contentType: "text/css",
+                contentEncoding: "gzip"
+            });
+        });
+    });
+}
+
+function uploadPluginIcon(data) {
+    return new Promise((resolve, reject) => {
+        upload({
+            resolve: resolve,
+            reject: reject,
+            key: "icon.png",
+            data: data,
+            contentType: "image/png"
+        });
+    });
+}
+
+function uploadMarkdown() {
+    return new Promise(resolve => {
+        fs.readFile("./markdown.md", (err, pluginMarkDown) => {
+            if (!err && pluginMarkDown) {
+                upload({
+                    resolve: resolve,
+                    reject: err => {
+                        console.log("💥  " + err);
+                        resolve(null);
+                    },
+                    key: "markdown.md",
+                    data: pluginMarkDown,
+                    contentType: "text/markdown"
+                });
+            } else {
+                resolve(null);
+            }
+        });
+    });
+}
+
+function uploadManifestJSON() {
+    return new Promise((resolve, reject) => {
+        upload({
+            resolve: resolve,
+            reject: reject,
+            key: "manifest.json",
+            data: JSON.stringify(manifest, null, 4),
+            contentType: "application/json"
+        });
+    });
+}
+
+function upload(params) {
+    s3Bucket.upload(
+        {
+            Bucket: args.bucket,
+            Key: baseKey + "/" + params.key,
+            Body: params.data,
+            ContentType: params.contentType,
+            ContentEncoding: params.contentEncoding || "UTF-8",
+            ACL: "public-read"
+        },
+        (err, data) => (err ? params.reject(err) : params.resolve(data.Location))
+    );
+}
